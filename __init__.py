617e82632b806c7319a9beec4eda33361a66ccaa
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-03-04 13:40:07 -0500

    Changes

--- __init__.py
+++ __init__.py
@@ -5,14 +5,16 @@ from login.routes import login
 from register.routes import register
 from logout.routes import logout
 
-def create_app():
-    app = Flask(__name__)
+## Initialize flask application
+app = Flask(__name__)
+app.secret_key = 'fywugeg9f3gof78'
 
-    app.secret_key = "fywugeg9f3gof78"
+# Blueprints
+app.register_blueprint(index)
+app.register_blueprint(register, url_prefix="/register")
+app.register_blueprint(login, url_prefix="/login")
+app.register_blueprint(logout, url_prefix="/logout")
 
-    app.register_blueprint(index)
-    app.register_blueprint(register, url_prefix="/register")
-    app.register_blueprint(login, url_prefix="/login")
-    app.register_blueprint(logout, url_prefix="/logout")
-
-    return app
+## Run
+if __name__ == '__main__':
+    app.run()
