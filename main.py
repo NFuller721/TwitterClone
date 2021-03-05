c1bcc29289489ffc963c9ace02f3fbce22e2b5d1
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-03-05 16:43:46 -0500

    DbNames

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Connection = Connection(
     host="127.0.0.1",
     username="Noah721",
     password="Satchel21",
-    databaseName="TwitterClone3"
+    databaseName="TwitterClone3Real"
 )
 
 def Start():
