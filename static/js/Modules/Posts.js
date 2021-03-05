b14dfabbd98f365190f97bc0c8bb8ef12c24cb5c
NFuller721 <74070284+NFuller721@users.noreply.github.com>
2021-03-04 20:02:47 -0500

    Got rid of console logs

--- static/js/Modules/Posts.js
+++ static/js/Modules/Posts.js
@@ -6,19 +6,15 @@ var posts = (GetPost, Post, body) => {
   GetPost().done((data) => {
     var Posts = '';
     for (var i = 0; i < data.Response.Posts.length; i++) {
-      console.log(i)
       var post = data.Response.Posts[i];
       var username = '';
-      console.log(i)
       for (var j = 0; j < data.Response.Users.length; ++j) {
         if (data.Response.Users[j][1] == post[2]) {
           username = data.Response.Users[j][0]
         }
       }
       Posts = Posts.concat(Post(postText=post[1], userid=post[2], username=username));
-      console.log(i)
       if (i+1 == data.Response.Posts.length) {
-        console.log("hello")
         body.append(`
           <div class="Posts">
             ${Posts}
